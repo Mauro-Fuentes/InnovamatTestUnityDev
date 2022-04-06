b8f8157ca7a0bbc03a5542c994904a54fd5c30d3
Mauro <maurofuentes@msn.com>
2022-04-06 09:05:59 +0200

    UI canvas and panels sketch - Overall organization and presentation - TextMeshPro essentials added

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class ColorChanger : MonoBehaviour
+{
+    public TMP_Text textMeshPro;
+
+    private void Start()
+    {
+        textMeshPro.color = new Color(0.2f, 0.5f, 0.4f, 0.2f);    
+    }
+}
---
+++ Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomNumberGenerator : MonoBehaviour
+{
+    private int minNumber;
+    private int maxNumber;
+
+    public int GenerateRandomNumber()
+    {
+        return Random.Range(minNumber, maxNumber);
+    }
+}
